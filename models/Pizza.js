9cb88f68c95642c7737ec0892f074dc35eebaab8
JacobReeder <jacob.reeder1@gmail.com>
2022-05-22 15:31:18 -0700

    Virtuals added

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+    {
+
     pizzaName: {
         type: String
     },
@@ -21,9 +23,27 @@ const PizzaSchema = new Schema({
         default: 'Large'
     },
 
-    toppings: []
-});
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+      ]
+},
+ {
+    toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 const Pizza = model('Pizza', PizzaSchema);
 
-module.exports = Pizza;
+module.exports = Pizza, Comment;
