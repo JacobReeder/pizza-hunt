2961455b61391177a2b111c636fb967c9c486485
JacobReeder <jacob.reeder1@gmail.com>
2022-05-25 18:17:28 -0700

    Reformated code

--- models/Pizza.js
+++ models/Pizza.js
@@ -44,9 +44,11 @@ const PizzaSchema = new Schema(
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce(
+        (total, comment) => total + comment.replies.length + 1, 
+        0);
   });
 
 const Pizza = model('Pizza', PizzaSchema);
 
-module.exports = Pizza, Comment;
+module.exports = Pizza;
