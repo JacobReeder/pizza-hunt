2420ef2d517ebe8f78007c2db802a44b93f79c40
JacobReeder <jacob.reeder1@gmail.com>
2022-05-27 15:37:19 -0700

    Validation added to Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,15 +5,18 @@ const dateFormat = require('../utils/dateFormat');
 const ReplySchema = new Schema(
   {
       // set custom id to avoid confusion with parent comment _id
-      replyId: {
+    replyId: {
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId()
       },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
